4d22f10794d6205d14cfe1ea9ffe901d96e1c9bc
MKaznacheev <kaznacheev.ma@gmail.com>
2025-02-26 21:04:33 +0400

    Add unit tests for BoundComparator
    Signed-off-by: Matvei Kaznacheev <kaznacheev.ma@gmail.com>

---
+++ src/test/java/xyz/derivora/intervalkit/test/bounds/comparison/BoundComparatorTest.java
@@ -0,0 +1,36 @@
+/*
+ * This file is part of Derivora Interval Kit.
+ *
+ * Derivora Interval Kit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Derivora Interval Kit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Derivora Interval Kit. If not, see https://www.gnu.org/licenses/lgpl-3.0.html.
+ */
+
+package xyz.derivora.intervalkit.test.bounds.comparison;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
+import xyz.derivora.intervalkit.bounds.Bound;
+import xyz.derivora.intervalkit.bounds.comparison.BoundComparator;
+import xyz.derivora.intervalkit.test.bounds.BoundComparisonTest;
+
+@Tag("xyz/derivora/intervalkit/bounds/comparison")
+@DisplayName("Tests for BoundComparator")
+class BoundComparatorTest extends BoundComparisonTest {
+
+    private static final BoundComparator BOUND_COMPARATOR = BoundComparator.getInstance();
+
+    @Override
+    protected int compare(Bound firstBound, Bound secondBound) {
+        return BOUND_COMPARATOR.compare(firstBound, secondBound);
+    }
+}
