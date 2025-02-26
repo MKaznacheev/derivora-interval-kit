0928637a4f8a0c04c58c05e6fd6ba9f1140df878
MKaznacheev <kaznacheev.ma@gmail.com>
2025-02-26 18:46:57 +0400

    Add module-info.java for the test module derivora.interval.kit.test.
    Signed-off-by: Matvei Kaznacheev <kaznacheev.ma@gmail.com>

---
+++ src/test/java/module-info.java
@@ -0,0 +1,22 @@
+/*
+ * This file is part of Derivora Interval Kit.
+ *
+ * Derivora Interval Kit is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Derivora Interval Kit is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Derivora Interval Kit. If not, see https://www.gnu.org/licenses/lgpl-3.0.html.
+ */
+
+open module derivora.interval.kit.test {
+    requires derivora.interval.kit;
+    requires derivora.util.kit;
+    requires org.junit.jupiter.params;
+}
